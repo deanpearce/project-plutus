af7defd89a48da518d322cf1552d8721869f76c7
Dean Pearce <deanpearce@gmail.com>
2018-03-23 23:19:13 -0400

    Import changes from PoC.

---
+++ components/service/budget-api/src/main/java/com/budget/service/api/budgetapi/controller/AccountController.java
@@ -0,0 +1,35 @@
+package com.budget.service.api.budgetapi.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account")
+@Api(value = "Account", description = "Account management and administration")
+public class AccountController {
+
+    @ApiOperation(value = "Create a new account", response = ResponseEntity.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
+    @PostMapping(
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Void> create(){
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+}
+
