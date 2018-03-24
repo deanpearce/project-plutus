639995f2b3082f964003bbb2470ac6070ff7aa0a
Dean Pearce <deanpearce@gmail.com>
2018-03-23 22:51:25 -0400

    Initial import of Spring Boot baseline project.

---
+++ components/service/budget-api/src/main/java/com/budget/service/api/budgetapi/BudgetApiApplication.java
@@ -0,0 +1,12 @@
+package com.budget.service.api.budgetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetApiApplication.class, args);
+	}
+}
